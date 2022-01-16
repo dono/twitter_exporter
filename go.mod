c1d06955b4a236090b3a88c1c91392809d93d041
dono <xononeet@gmail.com>
2022-01-17 01:01:37 +0900

    Init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dono/twitter_exporter
+
+go 1.17
+
+require github.com/g8rswimmer/go-twitter v1.1.4 // indirect
---
+++ twitter_exporter.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/g8rswimmer/go-twitter"
+)
+
+type authorize struct {
+	Token string
+}
+
+func (a authorize) Add(req *http.Request) {
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", a.Token))
+}
+
+
+func main() {
+	token := flag.String("token", "", "twitter API token")
+	query := flag.String("query", "", "twitter query")
+	flag.Parse()
+
+	tweet := &twitter.Tweet{
+		Authorizer: authorize{
+			Token: *token,
+		},
+		Client: http.DefaultClient,
+		Host:   "https://api.twitter.com",
+	}
+	fieldOpts := twitter.TweetFieldOptions{
+		TweetFields: []twitter.TweetField{twitter.TweetFieldCreatedAt, twitter.TweetFieldConversationID, twitter.TweetFieldLanguage},
+	}
+	searchOpts := twitter.TweetRecentSearchOptions{}
+
+	recentSearch, err := tweet.RecentSearch(context.Background(), *query, searchOpts, fieldOpts)
+	var tweetErr *twitter.TweetErrorResponse
+	switch {
+	case errors.As(err, &tweetErr):
+		printTweetError(tweetErr)
+	case err != nil:
+		fmt.Println(err)
+	default:
+		printRecentSearch(recentSearch)
+	}
+}
+
+func printRecentSearch(recentSearch *twitter.TweetRecentSearch) {
+	for _, lookup := range recentSearch.LookUps {
+		enc, err := json.MarshalIndent(lookup, "", "    ")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(enc))
+	}
+	enc, err := json.MarshalIndent(recentSearch.Meta, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(enc))
+
+}
+
+func printTweetError(tweetErr *twitter.TweetErrorResponse) {
+	enc, err := json.MarshalIndent(tweetErr, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(enc))
+}
